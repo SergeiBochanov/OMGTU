7607930374b7258d987032403ecb129894cadf32
Бочанов Сергей <144762247+SergeiBochanov@users.noreply.github.com>
2023-09-18 22:06:44 +0600

    Add files via upload

---
+++ Algoritmizaciya/2_18.09.23.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (k/3600 > 10)
+            {
+                Console.Write($"{k / 3600}:");
+            } else
+            {
+                Console.Write($"0{k / 3600}:");
+            }
+            if ((k%3600)/60 > 10)
+            {
+                Console.Write($"{(k % 3600) / 60}:");
+            } else
+            {
+                Console.Write($"0{(k % 3600) / 60}:");
+            }
+            if ((k % 3600) % 60 > 10)
+            {
+                Console.Write($"{(k % 3600) % 60}");
+            }
+            else
+            {
+                Console.Write($"0{k % 3600 % 60}");
+            }
+        }
+    }
+}
