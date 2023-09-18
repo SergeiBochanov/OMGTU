fb4e77621876cd977dbdf68bd69e0db5e258fbfa
Бочанов Сергей <144762247+SergeiBochanov@users.noreply.github.com>
2023-09-18 22:10:05 +0600

    Add files via upload

---
+++ Algoritmizaciya/3_18.09.23.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = Convert.ToInt32(Console.ReadLine());
+            float b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{(a + b) / 2 - Math.Abs(a - b) / 2} {(a + b) / 2 + Math.Abs(a - b) / 2}");
+        }
+    }
+}
